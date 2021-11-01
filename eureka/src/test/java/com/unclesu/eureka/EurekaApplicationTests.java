25ec264de9d56f08dd697e399c3bbaddfd44d57c
sugaofeng <sugaofeng>
2021-11-01 15:41:21 +0800

    feat: 注册中心
    注册中心选用 Eureka

---
+++ eureka/src/test/java/com/unclesu/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unclesu.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
